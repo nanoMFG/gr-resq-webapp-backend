79d7c22a7c5ec195bc50663214a7e3a801667f8c
Siddharth Sanghavi <sanghavisidd@gmail.com>
2022-03-22 18:05:54 -0500

    update share schema

--- app/schemas/Share.py
+++ app/schemas/Share.py
@@ -5,5 +5,5 @@ from uuid import UUID
 class Share(BaseModel):
   experimentID: int = Field(...)
   sender: UUID = Field(...)
-  recipients: list(UUID) = Field(...)
+  recipients: list[UUID] = Field(...)
   isRecipientGroup: bool = Field(False)
